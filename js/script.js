9fbd4f00e16b3256d6ae70717b3037cae5406c30
Anton <toxajlb@yandex.ru>
2020-06-19 17:20:01 +0300

    new commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
 'use strict';
+
+window.addEventListener('DOMContentLoaded', () => {
+
+    const tabs = document.querySelectorAll('.tabheader__item'),
+          tabsContent = document.querySelectorAll('.tabcontent'),
+          tabsParent = document.querySelectorAll('.tabheader__items');
+
+    function hideTabContent() {
+        tabsContent.forEach(item => {
+            item.style.display = 'none';
+        });
+
+        tabs.forEach(item => {
+            item.classList.remove('.tabheader__item_active');
+        });
+    }
+    function showTabContent(i = 0) {
+        tabsContent[i].style.display = 'block';
+        tabs[i].classList.add('.tabheader__item_active');
+    }
+
+    hideTabContent();
+    showTabContent();
+});
