2295486246e22afb51468ba0204cc2de622bfa27
Anton <toxajlb@yandex.ru>
2020-08-24 16:53:14 +0300

    modified index.html, script.js

--- js/script.js
+++ js/script.js
@@ -334,6 +334,7 @@ function showModalByScroll() {
     //Slider
 
     const slides = document.querySelectorAll('.offer__slide'),
+        slider = document.querySelector('.offer__slider'),
         prev = document.querySelector('.offer__slider-prev'),
         next = document.querySelector('.offer__slider-next'),
         total = document.querySelector('#total'),
@@ -362,12 +363,75 @@ function showModalByScroll() {
         slides.forEach(slide => {
             slide.style.width = width;
         });
+
+        slider.style.position = 'relative';
+
+        const indicators = document.createElement('ol'),
+              dots = [];
+
+        indicators.classList.add('carousel-indicators');
+        indicators.style.cssText = `
+            position: absolute;
+            right: 0;
+            bottom: 0;
+            left: 0;
+            z-index: 15;
+            display: flex;
+            justify-content: center;
+            margin-right: 15%;
+            margin-left: 15%;
+            list-style: none;
+        `;
+        slider.append(indicators);
+
+        for (let i = 0; i < slides.length; i++) {
+            const dot = document.createElement('li');
+            dot.setAttribute('data-slide-to', i + 1);
+            dot.style.cssText = `
+                box-sizing: content-box;
+                flex: 0 1 auto;
+                width: 30px;
+                height: 6px;
+                margin-right: 3px;
+                margin-left: 3px;
+                cursor: pointer;
+                background-color: #fff;
+                background-clip: padding-box;
+                border-top: 10px solid transparent;
+                border-bottom: 10px solid transparent;
+                opacity: .5;
+                transition: opacity .6s ease;
+            `;
+            indicators.append(dot);
+            dots.push(dot);
+
+            if(i == 0) {
+                dot.style.opacity = 1;
+            }
+        }
+
+        function selectedDots() {
+            dots.forEach(dot => dot.style.opacity = '.5');
+            dots[slideIndex - 1].style.opacity = 1;
+        }
+
+        function currentSlide() {
+            if(slides.length < 10) {
+                current.textContent = `0${slideIndex}`;
+            } else {
+                current.textContent = slideIndex;
+            }
+        }
+
+        function deleteNotDigits(str) {
+            return +str.replace(/\D/g, '');
+        }
     
         next.addEventListener('click', () => {
-            if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+            if (offset == deleteNotDigits(width) * (slides.length - 1)) {
                 offset = 0;
             } else {
-                offset += +width.slice(0, width.length - 2);
+                offset += deleteNotDigits(width);
             }
 
             slidesField.style.transform = `translateX(-${offset}px)`;
@@ -378,18 +442,15 @@ function showModalByScroll() {
                 slideIndex++;
             }
 
-            if(slides.length < 10) {
-                current.textContent = `0${slideIndex}`;
-            } else {
-                current.textContent = slideIndex;
-            }
+            currentSlide();
+            selectedDots();
         });
 
         prev.addEventListener('click', () => {
             if (offset == 0) {
-                offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+                offset = deleteNotDigits(width) * (slides.length - 1);
             } else {
-                offset -= +width.slice(0, width.length - 2);
+                offset -= deleteNotDigits(width);
             }
 
             slidesField.style.transform = `translateX(-${offset}px)`;
@@ -400,11 +461,21 @@ function showModalByScroll() {
                 slideIndex--;
             }
 
-            if(slides.length < 10) {
-                current.textContent = `0${slideIndex}`;
-            } else {
-                current.textContent = slideIndex;
-            }
+            currentSlide();
+            selectedDots();
+        });
+
+        dots.forEach(dot => {
+        dot.addEventListener('click', (e) => {
+            const slideTo = e.target.getAttribute('data-slide-to');
+
+            slideIndex = slideTo;
+            offset = deleteNotDigits(width) * (slideTo - 1);
+            slidesField.style.transform = `translateX(-${offset}px)`;
+
+            currentSlide();
+            selectedDots();
+            });
         });
     
 
@@ -447,5 +518,22 @@ function showModalByScroll() {
     // next.addEventListener('click', () => {
     //     plusSlides(1);
     // });
+
+    // Calc
+
+    const result = document.querySelector('.calculating__result span');
+    let sex, height, weight, age, ratio;
+
+    function calcTotal() {
+        if (!sex || !height || !weight || !age || !ratio) {
+            result.textContent = 'Заполните корректно все денные';
+            return;
+        }
+        
+        if (sex === 'female') {
+            result.textContent = (447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)) * ratio;
+        }else
+            result.textContent = (88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age)) * ratio;
+    }
 
 });
