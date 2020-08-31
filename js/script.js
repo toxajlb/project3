c482a76ffd1a4855003021765c57cc597b15c119
Anton <toxajlb@yandex.ru>
2020-08-31 16:47:23 +0300

    new commits

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 'use strict';
 
+require('es6-promise').polyfill();
+import 'nodelist-foreach-polyfill';
+
 import calc from './modules/calc';
 import cards from './modules/cards';
 import forms from './modules/forms';
